fabcbb070cfeb2c63a787e4e773acc387b66f0a3
VladimirShevelevich <vasiliy812@yandex.ru>
2018-02-02 12:02:54 +0200

    First initilization

---
+++ Assets/Demo/TestScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QAds;
+
+public class TestScript : MonoBehaviour {
+
+    public void ShowBanner()
+    {
+        QuickAds.instance.ShowBanner();
+    }
+
+    public void RemoveBanner()
+    {
+        QuickAds.instance.RemoveBanner();
+    }
+
+    public void ShowInter()
+    {
+        QuickAds.instance.ShowInterstitialAd();
+    }    
+
+    public void ShowRewarded()
+    {
+        QuickAds.instance.ShowRewardedAd();
+    }    
+
+
+
+    public void GetReward()
+    {
+        Debug.Log("Get Reward!");
+    }
+}
